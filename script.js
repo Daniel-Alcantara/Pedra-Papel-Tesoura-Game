0b44586bdfa629b1a2464aee0a484f9fdd27596c
daniel.alcantara <daniel.alcantara@4x4brasil.com.br>
2023-02-06 16:58:56 -0300

    aplicando 2 tela para resultado user

--- script.js
+++ script.js
@@ -7,16 +7,32 @@ $(document).ready(function () {
     var player_select;
 
     var bot_select;
+    var box_item_selecionado_bot;
 
     $(".papel_item").on("click", function () {
 
         player_select = papel
+
+        console.log($(this))
+        
+
+
 
     })
 
     $(".pedra_item").on("click", function () {
 
         player_select = pedra
+
+        var box_add = $(this)
+
+        Bot_select_item()
+
+        $(".selecionar_item").css("display", "none")
+
+
+        $(".box_item_selecionado .item_box").append(box_add)
+
 
     })
 
@@ -27,14 +43,20 @@ $(document).ready(function () {
     })
 
 
-
 })
 
 // Seleção do Bot
-function bot_select_item(){
+function Bot_select_item() {
 
     var ArrayItens = ['pedra', 'papel', 'tesoura'];
     var bot_select = ArrayItens[(Math.random() * ArrayItens.length) | 0]
     console.log(bot_select)
+
+}
+
+// Resultado do Jogo
+function Calcular_resultado() {
+
+
 
 }
